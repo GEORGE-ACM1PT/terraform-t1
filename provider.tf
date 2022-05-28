5ed9ad755729927457145c18d12996341768d27f
george <luiscarrillo129@gmail.com>
2022-05-28 12:35:26 -0600

    subnet associations

--- provider.tf
+++ provider.tf
@@ -103,3 +103,30 @@ resource "aws_route_table" "route_table_private" {
     Name = "george-private-route-table-tf"
   }
 }
+
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.public1.id
+  route_table_id = aws_route_table.route_table_public.id
+}
+resource "aws_route_table_association" "b" {
+  subnet_id      = aws_subnet.public2.id
+  route_table_id = aws_route_table.route_table_public.id
+}
+resource "aws_route_table_association" "c" {
+  subnet_id      = aws_subnet.public3.id
+  route_table_id = aws_route_table.route_table_public.id
+}
+
+resource "aws_route_table_association" "d" {
+  subnet_id      = aws_subnet.private1.id
+  route_table_id = aws_route_table.route_table_private.id
+}
+resource "aws_route_table_association" "e" {
+  subnet_id      = aws_subnet.private2.id
+  route_table_id = aws_route_table.route_table_private.id
+}
+resource "aws_route_table_association" "f" {
+  subnet_id      = aws_subnet.private3.id
+  route_table_id = aws_route_table.route_table_private.id
+}
